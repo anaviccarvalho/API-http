e659761275b5a95ec9acbe8cd3a50fd9cc81852e
anaviccarvalho <ana.carvalho19@estudante.ifms.edu.br>
2026-08-24 21:28:37 +0000

    feat: api criada e funcionando

---
+++ api.js
@@ -0,0 +1,56 @@
+import { error } from 'node:console';
+import http from 'node:http'
+
+const porta = 3000
+
+const tarefas = [
+    {id: 1, titulo: 'Lavar louça'},
+    {id: 2,titulo: 'Comprar uma RTX 5090'}
+]
+
+const server = http.createServer((requisicao, resposta) => {
+    resposta.setHeader('Content-Type','application/json; charset=utf-8')
+
+    if (requisicao.method == 'GET' && requisicao.url == '/tarefas') {
+        resposta.statusCode = 200
+        resposta.end(JSON.stringify(tarefas))
+    } else if (requisicao.method == 'POST' && requisicao.url == '/tarefa') {
+        let body = ''
+
+        requisicao.on('data', (chunk) => {
+            body += chunk.toString()
+        })
+
+        requisicao.on('end', () => {
+            try{
+                const movaTarefa = JSON.parse(body)
+
+                if (!novaTarefa.titulo) {
+                    resposta.statusCode = 400
+                    resposta.end(JSON.stringify({error: 'o campo "título" é obrigatório.'}));
+                }
+
+                const tarefaCriada = {
+                    id: tarefas.length + 1,
+                    titulo: novaTarefa.titulo
+                }
+
+                tarefa.push(tarefaCriada)
+
+                respostas.statusCode = 201
+                resposta.end(JSON.stringify(tarefaCriada))
+
+            } catch(error) {
+                resposta.statusCode = 400
+                resposta.end(JSON.stringify({error: 'Formato JSON inválido!'}))
+            }
+        })
+    } else {
+        resposta.statusCode = 404
+        resposta.end(JSON.stringify({error: 'Pagina não encontrada.'}))
+    }
+});
+
+server.listen(porta, () => {
+    console.log(`Servidor funcionando na porta ${porta}`);
+});
